04270e8b5674ca97578388f6db97b2e0fc98b525
Roxanne Krause <rlutz1@unm.edu>
2026-04-20 08:35:31 -0600

    moving some crap

---
+++ src/root/Drive.java
@@ -0,0 +1,19 @@
+/**
+ * ideo is to start demo from this puppy, a "driver" of the demo
+ *
+ * this will start the MAIN demo space.
+ */
+
+public class Driver {
+
+    /**
+     * main method to start up the demo.
+     * @param args command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("What's up, sluts.");
+    } // end main method
+
+} // end class
+
+
