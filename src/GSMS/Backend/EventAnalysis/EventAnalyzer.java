1d6a85d9a54e44e0b01e2ea8e581b1bea6e96960
Roxanne Krause <rlutz1@unm.edu>
2026-04-20 11:15:42 -0600

    adding in method sigs for event analysis

--- src/GSMS/Backend/EventAnalysis/EventAnalyzer.java
+++ src/GSMS/Backend/EventAnalysis/EventAnalyzer.java
@@ -6,4 +6,48 @@ package GSMS.Backend.EventAnalysis;
 
 public class EventAnalyzer {
 
+    public EventAnalyzer() {
+
+    } // end constructor
+
+    /**
+     * call to scan all rooms in the gym
+     * and retrieve data from various devices
+     */
+    public void scanRooms() {
+
+    } // end method
+
+    /**
+     * entry point to receive decibel levels collected
+     * from audio sensors in a given room
+     * @param roomId
+     */
+    public void getAudioData(String roomId) {
+
+    } // end method
+
+    /**
+     * entry point to receive video feed collected
+     * from video cameras in a given room
+     * @param roomId
+     */
+    public void getVideoData(String roomId) {
+
+    } // end method
+
+    /**
+     * entry point to receive a list of member vitals
+     * collected from wearable devices from all members in a given room.
+     * Additionally an entry point to receive signals from doorway sensors
+     * as read from the member wearables
+     * @param roomId
+     * @param member
+     */
+    public void getWearableData(String roomId, String member Id) {
+
+    } // end method
+
+
+
 } // end class
